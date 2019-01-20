99e2641b6c7531e8f4c6a38cab013f488527bc19
JeliZora <zoricajelic@yahoo.com>
2019-01-20 18:13:39 +0100

    'rtp'

---
+++ precache-manifest.f42115276b5ff5dd520e9c42898a4e13.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ea0291c4f855180ce010",
+    "url": "/static/js/main.ea0291c4.chunk.js"
+  },
+  {
+    "revision": "f4789f78bc5099d1ff6c",
+    "url": "/static/js/1.f4789f78.chunk.js"
+  },
+  {
+    "revision": "ea0291c4f855180ce010",
+    "url": "/static/css/main.e33efed9.chunk.css"
+  },
+  {
+    "revision": "5b2e5eb5b001e38583f628c8dd0b1792",
+    "url": "/index.html"
+  }
+];
